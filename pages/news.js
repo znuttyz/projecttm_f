77046030cc48ce4ac42ad3b222f6b5f088accc3e
oumho <oumho.p@gmail.com>
2018-05-04 10:33:54 +0700

    edit news

--- pages/news.js
+++ pages/news.js
@@ -7,15 +7,12 @@ class News extends Component {
 	render() {
 		return (
 			<div>
-<<<<<<< HEAD:pages/news.js
 				<Nav />
 				<AllBanner source="static/images/04-promotion/banner.jpg"/>
 				<NewsInfo />
-=======
 				<Nav isActive="หน้าแรก"/>
 				<AllBanner source="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F04-promotion%2Fbanner.jpg?alt=media&token=3b780257-265f-46d7-ae9f-b7992b23a858"/>
 				<News />
->>>>>>> master:pages/promotion.js
 				<Footer />
 			</div>
 		)

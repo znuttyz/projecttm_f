f0fb784137b0301d1db37b0bdb607c0571e23dae
znuttyz <n.phannurat@gmail.com>
2018-05-08 16:12:21 +0700

    add env config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,37 @@
+require('dotenv').config()                                                        
+const path = require('path')                                                      
+const Dotenv = require('dotenv-webpack')  
+
 const withSass = require('@zeit/next-sass')
+
 module.exports = withSass({
-<<<<<<< HEAD
-		poweredByHeader: false,
-		// exportPathMap: function () {
-		//     return {
-		//       '/': { page: '/soon' },
-		//       // '/t/home': { page: '/' },
-		//       // '/t/promotion': { page: '/promotion' },
-		//       // '/a/banner': { page: '/admin/banner' },
-		//       // '/a/banner/edit': { page: '/admin/banner_edit' },
-		//       // '/a/news': { page: '/admin/news' },
-		//       // '/a/promotion': { page: '/admin/promotion' }
-		//     }
-		// }
-=======
 	poweredByHeader: false,
-	// exportPathMap: function () {
-	//     return {
-	//       '/': { page: '/soon' },
-	//       '/t/home': { page: '/' },
-	//       '/t/news': { page: '/news' },
-	//       // '/t/promotion': { page: '/promotion' },
-	//       '/a': { page: '/admin' },
-	//       '/a/banner': { page: '/admin/banner' },
-	//       '/a/banner/edit': { page: '/admin/banner_edit' },
-	//       '/a/news': { page: '/admin/news' },
-	//       '/a/news/edit': { page: '/admin/banner_edit' },
-	//       '/a/promotion': { page: '/admin/promotion' },
-	//       '/a/promotion/edit': { page: '/admin/banner_edit' }
-	//     }
-	// }
->>>>>>> master
+	exportPathMap: function () {
+	    return {
+	      '/': { page: '/soon' },
+	      '/t/home': { page: '/' },
+	      '/t/news': { page: '/news' },
+	      // '/t/promotion': { page: '/promotion' },
+	      '/a': { page: '/admin' },
+	      '/a/banner': { page: '/admin/banner' },
+	      '/a/banner/edit': { page: '/admin/banner_edit' },
+	      '/a/news': { page: '/admin/news' },
+	      '/a/news/edit': { page: '/admin/banner_edit' },
+	      '/a/promotion': { page: '/admin/promotion' },
+	      '/a/promotion/edit': { page: '/admin/banner_edit' }
+	    }
+	},
+	webpack(config, options) {
+		config.plugins = config.plugins || []                                         
+	    config.plugins = [                                                            
+	    	...config.plugins,                                                          
+
+			// Read the .env file                                                       
+			new Dotenv({                                                                
+				path: path.join(__dirname, '.env'),                                       
+				systemvars: true                                                          
+			})                                                               
+	    ]                                                                             
+	    return config
+	}
 })
--- pages/actions/ConfigActions.js
+++ pages/actions/ConfigActions.js
@@ -1,17 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-
 import reducers from '../reducers'
 
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: process.env.FB_APIKEY,
+    authDomain: process.env.FB_AUTHDOMAIN,
+    databaseURL: process.env.FB_DATABASEURL,
+    projectId: process.env.FB_PROJECTID,
+    storageBucket: process.env.FB_STORAGEBUCKET,
+    messagingSenderId: process.env.FB_MESSAGINGSENDERID
  }
 
 if (!firebase.apps.length) {

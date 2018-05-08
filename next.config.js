6644f8182cdfe81ae1d6e55ee74809f1d2759feb
znuttyz <n.phannurat@gmail.com>
2018-05-09 01:40:17 +0700

    update component PageNumber

--- next.config.js
+++ next.config.js
@@ -6,21 +6,21 @@ const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
 	poweredByHeader: false,
-	exportPathMap: function () {
-	    return {
-	      '/': { page: '/soon' },
-	      '/t/home': { page: '/' },
-	      '/t/news': { page: '/news' },
-	      // '/t/promotion': { page: '/promotion' },
-	      '/a': { page: '/admin' },
-	      '/a/banner': { page: '/admin/banner' },
-	      '/a/banner/edit': { page: '/admin/banner_edit' },
-	      '/a/news': { page: '/admin/news' },
-	      '/a/news/edit': { page: '/admin/banner_edit' },
-	      '/a/promotion': { page: '/admin/promotion' },
-	      '/a/promotion/edit': { page: '/admin/banner_edit' }
-	    }
-	},
+	// exportPathMap: function () {
+	//     return {
+	//       '/': { page: '/soon' },
+	//       '/t/home': { page: '/' },
+	//       '/t/news': { page: '/news' },
+	//       // '/t/promotion': { page: '/promotion' },
+	//       '/a': { page: '/admin' },
+	//       '/a/banner': { page: '/admin/banner' },
+	//       '/a/banner/edit': { page: '/admin/banner_edit' },
+	//       '/a/news': { page: '/admin/news' },
+	//       '/a/news/edit': { page: '/admin/banner_edit' },
+	//       '/a/promotion': { page: '/admin/promotion' },
+	//       '/a/promotion/edit': { page: '/admin/banner_edit' }
+	//     }
+	// },
 	webpack(config, options) {
 		config.plugins = config.plugins || []                                         
 	    config.plugins = [                                                            
--- pages/components/PageNumber.js
+++ pages/components/PageNumber.js
@@ -1,12 +1,62 @@
-const PageNumber = () => (
-	<div className="pagenumber clear">
-		<ul>
-			<li><a className="active">1</a></li>
-			<li><a>2</a></li>
-			<li><a>3</a></li>
-			<li><a>ต่อไป</a></li>
-		</ul>
-	</div>
-)
+import React, { Component } from 'react'
+
+class PageNumber extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			selectedPage: 1,
+			totalPage: 4,
+		}
+	}
+
+	_onSelectPage(selectedPage) {
+		this.setState({ selectedPage })
+	}
+
+	_onSelectNextPage() {
+		let selectedPage = this.state.selectedPage + 1
+		this.setState({ selectedPage })
+	}
+
+	_onSelectPreviousPage() {
+		let selectedPage = this.state.selectedPage - 1
+		this.setState({ selectedPage })
+	}
+
+	nextPage() {
+		if(this.state.selectedPage === this.state.totalPage) return;
+		else return <li><a onClick={()=>this._onSelectNextPage()}>ต่อไป</a></li>
+	}
+
+	previousPage() {
+		if(this.state.selectedPage === 1) return;
+		else return <li><a onClick={()=>this._onSelectPreviousPage()}>ก่อนหน้า</a></li>
+	}
+
+	showPage() {
+		let pages = []
+		for(let i = 1; i <= this.state.totalPage; i++) {
+			pages.push(<li><a className={(this.state.selectedPage === i)? "active":""} onClick={()=>this._onSelectPage(i)}>{i}</a></li>)
+		}
+		return pages
+	}
+
+	render() {
+		return (
+			<div className="pagenumber clear">
+				<ul>
+					{this.previousPage()}
+					{this.showPage()}
+					{this.nextPage()}
+				</ul>
+			</div>
+		)
+	}
+}
 
 export { PageNumber }
+
+// <li><a className={(this.state.selectedPage === 1)? "active":""} onClick={()=>this._onSelectPage(1)}>1</a></li>
+					// <li><a className={(this.state.selectedPage === 2)? "active":""} onClick={()=>this._onSelectPage(2)}>2</a></li>
+					// <li><a className={(this.state.selectedPage === 3)? "active":""} onClick={()=>this._onSelectPage(3)}>3</a></li>

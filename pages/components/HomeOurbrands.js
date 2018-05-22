6ac80b14efd6e0ed2af7bfd62508b1df415767e4
oumho <oumho.p@gmail.com>
2018-05-23 00:53:31 +0700

    add id to each brands

--- pages/components/HomeOurbrands.js
+++ pages/components/HomeOurbrands.js
@@ -8,7 +8,7 @@ const HomeOurbrands = () => (
 			</div>
 			<div className="home-ourbrands-inner">
 				<ul>
-					<li className="each-brand">
+					<li id="each-brand">
 						<Link href="/brands/tummour">
 							<a>
 								<div className="brandname">
@@ -20,7 +20,7 @@ const HomeOurbrands = () => (
 							</a>
 						</Link>
 					</li>
-					<li className="each-brand">
+					<li id="each-brand">
 						<Link href="/brands/laoyuan">
 							<a>
 								<div className="brandname">
@@ -32,7 +32,7 @@ const HomeOurbrands = () => (
 							</a>
 						</Link>
 					</li>
-					<li className="each-brand">
+					<li id="each-brand">
 						<Link href="/brands/jaewhon">
 							<a>
 								<div className="brandname">
@@ -44,7 +44,7 @@ const HomeOurbrands = () => (
 							</a>
 						</Link>
 					</li>
-					<li className="each-brand">
+					<li id="each-brand">
 						<Link href="/brands/pho">
 							<a>
 								<div className="brandname">

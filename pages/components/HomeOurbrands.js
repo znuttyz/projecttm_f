eec5c3baafb8ae54292f6230f1795330eb2acc25
oumho <oumho.p@gmail.com>
2018-05-23 00:38:27 +0700

    home ourbrands recode

--- pages/components/HomeOurbrands.js
+++ pages/components/HomeOurbrands.js
@@ -9,28 +9,52 @@ const HomeOurbrands = () => (
 			<div className="home-ourbrands-inner">
 				<ul>
 					<li className="each-brand">
-						<ul>
-							<li className="brandname"><Link href="/brands/tummour"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Ftm.png?alt=media&token=ac35c520-2402-422d-ba6b-c28a94eab001" /></a></Link></li>
-							<li className="brandimg"><Link href="/brands/tummour"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F01.jpg?alt=media&token=838b4a56-fc84-4c31-b4c4-1884cf61da6a" /></a></Link></li>
-				 		</ul>
+						<Link href="/brands/tummour">
+							<a>
+								<div className="brandname">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Ftm.png?alt=media&token=ac35c520-2402-422d-ba6b-c28a94eab001" />
+								</div>
+								<div className="brandimg">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F01.jpg?alt=media&token=838b4a56-fc84-4c31-b4c4-1884cf61da6a" />
+								</div>
+							</a>
+						</Link>
 					</li>
 					<li className="each-brand">
-						<ul>
-							<li className="brandname"><Link href="/brands/laoyuan"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Fly.png?alt=media&token=86762e41-6548-406f-b754-93171fd039dd" /></a></Link></li>
-							<li className="brandimg"><Link href="/brands/laoyuan"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F02.jpg?alt=media&token=6f52e4e2-f410-4bb3-a41a-3493689d2048" /></a></Link></li>
-						</ul>
+						<Link href="/brands/laoyuan">
+							<a>
+								<div className="brandname">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Fly.png?alt=media&token=86762e41-6548-406f-b754-93171fd039dd" />
+								</div>
+								<div className="brandimg">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F02.jpg?alt=media&token=6f52e4e2-f410-4bb3-a41a-3493689d2048" />
+								</div>
+							</a>
+						</Link>
 					</li>
 					<li className="each-brand">
-						<ul>
-							<li className="brandname"><Link href="/brands/jaewhon"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Fjh.png?alt=media&token=45764deb-998b-4ca7-82d7-d0881cfc2e5c" /></a></Link></li>
-							<li className="brandimg"><Link href="/brands/jaewhon"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F03.jpg?alt=media&token=7d7a69c5-63b0-47e0-b3c9-1b584d6c7216" /></a></Link></li>
-						</ul>
+						<Link href="/brands/jaewhon">
+							<a>
+								<div className="brandname">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Fjh.png?alt=media&token=45764deb-998b-4ca7-82d7-d0881cfc2e5c" />
+								</div>
+								<div className="brandimg">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F03.jpg?alt=media&token=7d7a69c5-63b0-47e0-b3c9-1b584d6c7216" />
+								</div>
+							</a>
+						</Link>
 					</li>
 					<li className="each-brand">
-						<ul>
-							<li className="brandname"><Link href="/brands/pho"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Fph.png?alt=media&token=8e38d452-66c5-4967-b04a-c2be46cc621a" /></a></Link></li>
-							<li className="brandimg"><Link href="/brands/pho"><a><img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F04.jpg?alt=media&token=9e22a7da-b50d-4d0b-a676-d95066c22b36" /></a></Link></li>
-						</ul>
+						<Link href="/brands/pho">
+							<a>
+								<div className="brandname">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F00-global%2Fph.png?alt=media&token=8e38d452-66c5-4967-b04a-c2be46cc621a" />
+								</div>
+								<div className="brandimg">
+									<img src="https://firebasestorage.googleapis.com/v0/b/tummour-original.appspot.com/o/static%2Fimages%2F01-home%2F04.jpg?alt=media&token=9e22a7da-b50d-4d0b-a676-d95066c22b36" />
+								</div>
+							</a>
+						</Link>
 					</li>
 				</ul>
 			</div>

e099f6a089e05e9a06dbce0dba73388da6323db2
znuttyz <n.phannurat@gmail.com>
2018-08-09 18:25:56 +0700

    update contact franchise link

--- pages/components/ContactFranchise.js
+++ pages/components/ContactFranchise.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import React, { Component } from "react";
 
 const ContactFranchise = ({ content }) => (
@@ -7,8 +6,8 @@ const ContactFranchise = ({ content }) => (
 			<img src={content.franchise.title} />
 			<h1>{content.franchise.tel}</h1>
 			<div className="franchise-btn">
-				<Link href=""><a>{content.franchise.button1}</a></Link>
-				<Link href=""><a>{content.franchise.button2}</a></Link>
+				<a href={content.franchise.button1_url} target="_blank">{content.franchise.button1}</a>
+				<a href={content.franchise.button2_url} target="_blank">{content.franchise.button2}</a>
 			</div>
 			<h2>{content.franchise.text}</h2>
 		</div>

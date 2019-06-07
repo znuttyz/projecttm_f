48f88361d60438d6bf48428a7da81aed2d29c968
Nuttapon Phannurat <nut@znuttyz.local>
2019-06-08 00:55:25 +0700

    update analytic and contact phone no.

--- pages/components/Analytics.js
+++ pages/components/Analytics.js
@@ -5,11 +5,13 @@ import ReactPixel from 'react-facebook-pixel';
 class Analytics extends Component {
 
 	componentDidMount() {
-		console.log(window.location.pathname + window.location.search)
-		ReactGA.initialize(process.env.GA_ID)
+		// console.log(window.location.pathname + window.location.search)
+		// ReactGA.initialize(process.env.GA_ID2)
+		ReactGA.initialize([{ trackingId: process.env.GA_ID, gaOptions: { name: 'tracker1' } }, 
+							{ trackingId: process.env.GA_ID2, gaOptions: { name: 'tracker2' } }])
 		ReactPixel.init(process.env.FB_PIXEL_ID);
 		setTimeout(()=> {
-			ReactGA.pageview(window.location.pathname + window.location.search)
+			ReactGA.pageview(window.location.pathname + window.location.search, ['tracker1', 'tracker2'])
 			ReactPixel.pageView()
 		})
 	}
